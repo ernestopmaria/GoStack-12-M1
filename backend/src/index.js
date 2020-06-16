d57d24d03eb1e15183baa06aee0c292d0ce34218
Ernesto Maria <ernestomaria93@gmail.com>
2020-06-16 05:46:27 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,5 +7,41 @@ app.listen(3333, ( )=>{
     console.log('🚀 Backend started!')
 });
 app.get('/projects' , (request, response)=>{
-    return response.json({mensagem:'Revisão teste agora'});
+    return response.json([
+        'Projecto',
+        'Projecto 2',
+    ]);
+
+    
 });
+
+app.post('/projects' , (request, response)=>{
+    return response.json([
+        'Projecto',
+        'Projecto 2',
+        'Projecto 3'
+    ]);
+
+    
+});
+
+app.put('/projects/:id' , (request, response)=>{
+    return response.json([
+        'Projecto',
+        'Projecto 2',
+        'Projecto 3'
+    ]);
+
+    
+});
+
+app.delete('/projects' , (request, response)=>{
+    return response.json([
+        'Projecto',
+        'Projecto 2',
+        'Projecto 3'
+    ]);
+
+    
+})
+
