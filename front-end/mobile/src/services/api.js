cd27762cf392c489eddb58830e11435092b2fd7c
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-06-28 16:48:30 +0100

    update

--- front-end/mobile/src/services/api.js
+++ front-end/mobile/src/services/api.js
@@ -1,13 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://10.0.2.2:3333/",
+  baseURL: "http://10.0.2.2:3333/",
 });
 
 export default api;
-
-/*
-se for um emulador pode inser o localhost utilizando o adb reverse tcp:3333 tcp:3333, caso
-não funcione podemos usar o ip: 10.0.2.2  
-se for num dispositivo fisico use o ip da maquina,
-*/
